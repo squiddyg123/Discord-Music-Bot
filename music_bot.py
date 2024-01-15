ae6bb293bd1dd0540bf5d4673ba1823f9ac4417b
squiddyg123 <156715617+squiddyg123@users.noreply.github.com>
2024-01-15 14:33:16 -0600

    Create music_bot.py
    main function to run bot

---
+++ music_bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os, asyncio
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+async def main():
+    async with bot:
+        await bot.add_cog(help_cog(bot))
+        await bot.add_cog(music_cog(bot))
+        await bot.start(os.getenv('TOKEN'))
+
+asyncio.run(main())
